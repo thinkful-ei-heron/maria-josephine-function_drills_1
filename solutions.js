871e17a86723d17e21d233a1ff87052d031752e4
Maria <mal3905@gmail.com>
2019-09-16 17:41:20 -0400

    #6

--- solutions.js
+++ solutions.js
@@ -1,4 +1,8 @@
 
+function getYearOfBirth(age){
+    return 2019 - age;
+}
+
 function createGreeting (name, age ){ 
     const yearOfBirth = 2019 - age;
     return `Hi, my name is Josephine and I\'m 22 years old. I was born in ${yearOfBirth}.`; 
