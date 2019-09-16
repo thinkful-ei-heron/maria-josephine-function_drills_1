befbeb685866d9dfab68729503b2525aca863de8
lilmaifun <lilmaifun@gmail.com>
2019-09-16 14:38:08 -0700

    #5

--- solutions.js
+++ solutions.js
@@ -1,6 +1,7 @@
 
 function createGreeting (name, age ){ 
-    return 'Hi, my name is Josephine and I\'m 22 years old'; 
+    const yearOfBirth = 2019 - age;
+    return `Hi, my name is Josephine and I\'m 22 years old. I was born in ${yearOfBirth}.`; 
 };
 
 const greeting1 = createGreeting();
